de044e77ae7deab5708bec1b7df418547584cd1b
gpaniati <gpaniati@gmail.com>
2023-07-10 08:41:19 -0300

    Incorporar Carrito

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,8 +1,22 @@
+import { Cart } from "react-bootstrap-icons";
+import "../css/NavBar.css";
+import { useState } from 'react';
+
 function CartWidged() {
-    return (
-      
-    );
-  }
-  
-  export default NavBar;
-  
+  const [contadorCarrito, setContadorCarrito] = useState(0);
+
+  const handleChangeGlobal = () => {
+    setContadorCarrito(contadorCarrito + 1);
+  };
+
+  return (
+    <div>
+      <ul className="listCarrito">
+        <li><Cart size={35} /></li>
+        <li>{contadorCarrito}</li>
+      </ul>
+    </div>
+  );
+}
+
+export default CartWidged;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,13 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import "../css/NavBar.css";
 import logoModoFit from "../images/logo_modofit_market.png"
+import CartWidged from "./CartWidget";
+import "../css/NavBar.css";
 
 function NavBar() {
-  return (
+
+   return (
     <Navbar expand="lg" className="barraNavegacion">    
       <Container>
         <Navbar.Brand idName="logoNavBar" href="#logo" style ={{ marginRight: 100}}>
@@ -28,9 +30,10 @@ function NavBar() {
               <NavDropdown.Item href="#linkDesayuno3">Miel</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="#link">Cosmética Natural</Nav.Link>
-            <Nav.Link href="#link">Contacto</Nav.Link>
+            <Nav.Link href="#link">Contacto</Nav.Link> 
           </Nav>
         </Navbar.Collapse>
+        <CartWidged/>
       </Container>
     </Navbar>
 
