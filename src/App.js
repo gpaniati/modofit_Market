572a09964eca86b71f1c78843aa45d7339c6a856
gpaniati <gpaniati@gmail.com>
2023-07-23 19:45:55 -0300

    Agrego Route /home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./components/home";
+
 
 function App() {
 
   return (
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
         <NavBar />
         <ItemListContainer saludo="Tienda de alimentos naturales"/>
       </header>
     </div>
+      <Routes>
+        <Route exact path="/home" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import "../css/Components.css";
+
+function Home() {
+
+  return (
+    <h1 className="h2Saludo">{saludo}</h1>
+  );
+}
+
+export default Home;
