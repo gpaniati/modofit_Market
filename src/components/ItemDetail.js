6e29d115284787f44472c88637c2e856e871b5fb
gpaniati <gpaniati@gmail.com>
2023-07-26 17:35:26 -0300

    Defino estructura ItemDetail

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,10 +1,19 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-function ItemDetail ({producto}) {
-    return (
-        <div></div>
-    )
+function ItemDetail({ producto }) {
+  return (
+    <div>
+      <Card style={{ width: "40rem" }}>
+        <Card.Img variant="top" src={producto.imagen} />
+        <Card.Body>
+          <Card.Title>{producto.nombre}</Card.Title>
+          <Card.Text>{producto.descripcion}</Card.Text>
+          <Card.Text>${producto.precio}</Card.Text>
+          <Button variant="primary">Comprar</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
-
 export default ItemDetail;
