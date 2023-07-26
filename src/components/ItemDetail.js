109094f9ef2f268a96bc7edda93c798da7fba2fd
gpaniati <gpaniati@gmail.com>
2023-07-26 17:34:15 -0300

    Declaro ItemDetail

---
+++ src/components/ItemDetail.js
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function ItemDetail ({producto}) {
+    return (
+        <div></div>
+    )
+}
+
+export default ItemDetail;
