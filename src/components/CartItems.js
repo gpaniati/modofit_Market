2deaf9224199613531339528be9cce0ceab22a15
gpaniati <gpaniati@gmail.com>
2023-08-19 15:16:58 -0300

    Items de carrito version 1.2

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,7 +1,33 @@
-import { Button, Card, Col, Container, Row, Image } from "react-bootstrap";
-import React from "react";
+//import { Button, Card, Col, Container, Row, Image } from "react-bootstrap";
+import Badge from 'react-bootstrap/Badge';
+import Image from 'react-bootstrap/Image';
+import { BsFillTrashFill } from "react-icons/bs";
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function CartItems({ item, itemDelete }) {
+
+  return (
+    <ListGroup as="ol">
+      <ListGroup.Item
+        as="li"
+        className="d-flex justify-content-around align-items-center"
+      >
+        <div className="ms-2 me-auto">
+          <div className="fw-bold">{item.nombre}</div>
+          ${item.precio}
+        </div>
+        <Image className="imageCart" src={item.imagen}/>
+        <Badge className="badgeCart" bg="success"pill>
+          {item.cantidad}
+        </Badge>
+        <BsFillTrashFill color="red" size={30} onClick={() => itemDelete(item.id)} />
+      </ListGroup.Item>
+    </ListGroup>
+  );
+}
+
+export default CartItems;
+/*
   return (
     <Card>
       <Container>
@@ -25,6 +51,4 @@ function CartItems({ item, itemDelete }) {
       </Container>
     </Card>
   );
-}
-
-export default CartItems;
+*/
